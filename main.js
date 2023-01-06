3ee21af66bbf6301f51a123d0cfe3c4deb4fda61
Patryk Hirny <pathirny@googlemail.com>
2023-01-06 14:34:51 +0000

    added buttons to each project and js comments

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-let first = document.querySelector('#pro-btn');
-let para = document.querySelector('#text');
-let container = document.querySelector('#cont');
-let hidden = document.querySelector('#hidden');
+// first button
+let first = document.querySelector('#show1');
+let para = document.getElementById('text1');
+let container = document.querySelector('#cont1');
+let hidden = document.querySelector('#hide1');
 
+// show description
 first.addEventListener('click', function(){
-    para = document.getElementById('text').style.display = 'block';
-    first = document.getElementById('pro-btn').style.display = 'none';
-    container = document.getElementById('cont').style.height = '250px';
-    hidden = document.getElementById('hidden').style.display = 'block';
-})
+    para = document.getElementById('text1').style.display = 'block';
+    first = document.getElementById('show1').style.display = 'none';
+    container = document.getElementById('cont1').style.height = '250px';
+    hidden = document.getElementById('hide1').style.display = 'block';
+});
+
+// hide description
+hide1.addEventListener('click', function(){
+    para = document.getElementById('text1').style.display = 'none';
+    first = document.getElementById('show1').style.display = 'block';
+    container = document.getElementById('cont1').style.height = '66px';
+    hidden = document.getElementById('hide1').style.display = 'none';
+});
 
